952ec42492c82484a20af717fcc7f853d4aba5bb
REZAN <asifrezan@gmail.com>
2022-01-09 21:23:01 +0600

    main function added

--- TestClass.java
+++ TestClass.java
@@ -1,4 +1,21 @@
 class testClass
 {
     private static testClass tc;
+
+
+    public static void main(String[] args) {
+        if(tc==null)
+        {
+            tc=newTestClass(id, name);
+            return;
+
+        }
+    }
 }
+
+static testClass (int id, string name)
+{
+    this.id=id;
+    this.name=name;
+}
+
